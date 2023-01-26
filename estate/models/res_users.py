a615e7e353a86db4ff6775bc7335495e3b6a786a
Damian Osowski <damian1.osowski@gmail.com>
2023-01-26 23:12:46 +0100

    Revert "[new] estate module (tutorial)"
    This reverts commit f1f2597a302be161ee0e766e33299e2bf327f500.

--- estate/models/res_users.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models, api
-
-
-class Users(models.Model):
-    _inherit = "res.users"
-
-    property_ids = fields.One2many("estate.property", 'salesperson_id', string='Properties')
